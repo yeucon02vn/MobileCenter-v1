1ae7718e6082ca9d112f8bd9ea7d73e25abe799a
DoThanhLoc <loc.do@nextfunc.com>
2020-07-09 17:12:30 +0700

    done

--- MobileCenter/View/ThemDonHang.aspx.designer.cs
+++ MobileCenter/View/ThemDonHang.aspx.designer.cs
@@ -42,21 +42,21 @@ namespace MobileCenter.View
         protected global::System.Web.UI.WebControls.Label lblTotal;
 
         /// <summary>
-        /// ImageButtonTieptucmuahang control.
+        /// LinkButton1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ImageButtonTieptucmuahang;
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
 
         /// <summary>
-        /// ImageButtonTaovaguidonhang control.
+        /// ImageButtonXacnhanthanhtoan control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ImageButtonTaovaguidonhang;
+        protected global::System.Web.UI.WebControls.LinkButton ImageButtonXacnhanthanhtoan;
     }
 }
